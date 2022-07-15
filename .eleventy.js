745461a00955286206990f81b372bda08ab0ae83
Farrel R <rafirizqy12@gmail.com>
2022-07-16 00:58:40 +0700

    Add passthrough copy for netlify redirects file

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,7 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.setLiquidOptions({
         dynamicPartials: false
     });
+    
+    // Temporary(?)
+    eleventyConfig.addPassthroughCopy('_redirects');
 }
