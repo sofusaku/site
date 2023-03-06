87c95c31f53ea1d4461443148321f1e17ba0bfa7
Farrel R <comfyte@MFR-MBA2020.local>
2023-03-06 20:28:18 +0700

    Bootstrap a fresh Gatsby project

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = (eleventyConfig) => {
-    eleventyConfig.setLiquidOptions({
-        dynamicPartials: false
-    });
-    
-    // Temporary(?)
-    eleventyConfig.addPassthroughCopy('_redirects');
-}
